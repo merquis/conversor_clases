b491f24a45537e5cb19e10e3376dc2deefa5358f
Merquis <merquis85@gmail.com>
2015-12-04 07:00:09 +0000

    Probando tests

--- tests/tests.js
+++ tests/tests.js
@@ -3,17 +3,17 @@ var assert = chai.assert;
 suite('Calcular', function() {
     test('32F = 0C', function() {
         original.value = "32F";
-        calculate();
+        Calcular();
         assert.deepEqual(converted.innerHTML, "0.0 Celsius");
     });
     test('45C = 113.0 Farenheit', function() {
         original.value = "45C";
-        calculate();
+        Calcular();
         assert.deepEqual(converted.innerHTML, "113.0 Farenheit");
     });
     test('5X = error', function() {
         original.value = "5X";
-        calculate();
+        //Calcular();
         assert.match(converted.innerHTML, /ERROR/);
     });
 });
