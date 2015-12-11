a774ac8b8463c70fb990782833c1bd05d88b9f7c
Merquis <merquis85@gmail.com>
2015-12-11 05:01:18 +0000

    probando

--- clases/medidas.js
+++ clases/medidas.js
@@ -6,13 +6,13 @@ Calcular.prototype.n1 = a; //propiedad numero 1
 Calcular.prototype.n2 = b; //propiedad numero 2
 function ConvertirCelsius() { //función para crear método convertir celsius
           if (num2 == 'c' || num2 == 'C'){
-        	 solucion = this.n1;        	 
+        	 solucion = this.n1;
 		  }
 		  else if (num2 == 'f' || num2 == 'F'){
-        	 solucion = (this.n1 - 32)*5/9;       	 
+        	 solucion = (this.n1 - 32)*5/9;
 		  }
 		  else if (num2 == 'k' || num2 == 'K'){
-        	 solucion = (this.n1*1) - 273.15       	 
+        	 solucion = (this.n1*1) - 273.15
 		  }
 		  return solucion;
 }
@@ -23,57 +23,57 @@ function Celsius(){ // función que muestra el resultado para Celsius
          operacion = "Resultado: "; //texto para pantalla
          escribir(); //función que escribe el resultado en pantalla.
          }
-		 
-		 
-function ConvertirFahrenheit() { 
+
+
+function ConvertirFahrenheit() {
 		if (num2 == 'c' || num2 == 'C'){
-					 solucion = (this.n1 * 9/5)+32;  
-					 return solucion;      	 
+					 solucion = (this.n1 * 9/5)+32;
+					 return solucion;
 		}
 		 else if (num2 == 'f' || num2 == 'F'){
-        	 solucion = this.n1;       	 
+        	 solucion = this.n1;
 		  }
 		 else if (num2 == 'k' || num2 == 'K'){
-        	 solucion = (1.8*(this.n1 - 273))+32;   	 
+        	 solucion = (1.8*(this.n1 - 273))+32;
 		  }
 		  return solucion;
-         
-				  
-}        
-Calcular.prototype.ConvertirFahrenheit = ConvertirFahrenheit 
-function Fahrenheit() { 
+
+
+}
+Calcular.prototype.ConvertirFahrenheit = ConvertirFahrenheit
+function Fahrenheit() {
          instanciar();
          result = numeros.ConvertirFahrenheit();
-         operacion = "Area del círculo: ";
+         operacion = "Resultado: ";
          escribir();
 }
-		 
-		 
-		 
-function ConvertirKelvin() { 
+
+
+
+function ConvertirKelvin() {
 		if (num2 == 'c' || num2 == 'C'){
 					 solucion = (this.n1*1)+273.15;
-					 return solucion;      	 
+					 return solucion;
 		}
 		  else if (num2 == 'f' || num2 == 'F'){
-        	 solucion = (this.n1*1 + 459.67) / 1.8;  	 
+        	 solucion = (this.n1*1 + 459.67) / 1.8;
 		  }
 		  else if (num2 == 'k' || num2 == 'K'){
-        	 solucion = this.n1;       	 
+        	 solucion = this.n1;
 		  }
 		  return solucion;
-				  
+
 }
-Calcular.prototype.ConvertirKelvin = ConvertirKelvin 
+Calcular.prototype.ConvertirKelvin = ConvertirKelvin
 function Kelvin() {
          instanciar()
          result = numeros.ConvertirKelvin()
-         operacion = "Area del triángulo: "
+         operacion = "Resultado: ";
          escribir()
 }
-		 
-		 
-		 
+
+
+
 function instanciar(){ //función que crea un objeto y asigna valores a las propiedades.
          num1 = document.calcular.num1.value
          num2 = document.calcular.num2.value
@@ -81,7 +81,7 @@ function instanciar(){ //función que crea un objeto y asigna valores a las prop
          numeros.n1 = num1
          numeros.n2 = num2
          return numeros
-}		 
+}
 function escribir() { //escribir resultados en el documento.
          texto = document.getElementById("respuesta")
          texto.innerHTML = operacion +  result
