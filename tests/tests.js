729357baad851f6190a78a6be196888e58802a61
Merquis <merquis85@gmail.com>
2015-12-11 07:07:56 +0000

    Cambios tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,20 +1,19 @@
 var assert = chai.assert;
 
-suite('Calcular', function() {
+suite('Conversor temperaturas', function() {
     test('32F = 0C', function() {
         medida_in.value = "32F";
-        Calcular();
-        assert.deepEqual(converted.innerHTML, "0.0 Celsius");
+        instanciar();
+        assert.deepEqual(respuesta.innerHTML, "0");
     });
     test('45C = 113.0 Farenheit', function() {
         medida_in.value = "45C";
-        Calcular();
-        assert.deepEqual(converted.innerHTML, "113.0 Farenheit");
+        instanciar();
+        assert.deepEqual(respuesta.innerHTML, "113");
     });
     test('5X = error', function() {
         medida_in.value = "5X";
-        Calcular();
-        assert.match(converted.innerHTML, /ERROR/);
+        instanciar();
+        assert.match(respuesta.innerHTML, /ERROR/);
     });
 });
-
