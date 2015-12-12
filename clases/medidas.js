1f7324ef2b0b7e64a8af42ffab767968f45b698d
Merquis <merquis85@gmail.com>
2015-12-12 00:01:25 +0000

    Código reescrito, eliminadas variables globales y realizados test correctamente

--- clases/medidas.js
+++ clases/medidas.js
@@ -1,101 +1,92 @@
-a=0;b=0;
-function Calcular() {
- 
-         }
-Calcular.prototype.n1 = a; //propiedad numero 1
-Calcular.prototype.n2 = b; //propiedad numero 2
+//a=0;b=0;
+function Calcular(temp) {
+  var regexp = /([-+]?\d+(?:\.\d*)?)\s*([cCkKfF])/;
+  var m = temp.match(regexp);
+  if (m) {
+    var num = m[1];
+    num = parseFloat(num);
+    var tipo = m[2];
+
+    this.n1 = num;
+    this.n2 = tipo;
+
+  }
+
+}
+Calcular.prototype.n1 = 0; //propiedad numero 1
+Calcular.prototype.n2 = 0; //propiedad numero 2
+
 function ConvertirCelsius() { //función para crear método convertir celsius
-          if (num2 == 'c' || num2 == 'C'){
-        	 solucion = this.n1;
-		  }
-		  else if (num2 == 'f' || num2 == 'F'){
-        	 solucion = (this.n1 - 32)*5/9;
-		  }
-		  else if (num2 == 'k' || num2 == 'K'){
-        	 solucion = (this.n1*1) - 273.15
-		  }
-		  return solucion;
+  var solucion;
+  if (this.n2 == 'c' || this.n2 == 'C'){
+    solucion = this.n1;
+  }
+  else if (this.n2 == 'f' || this.n2 == 'F'){
+    solucion = (this.n1 - 32)*5/9;
+  }
+  else if (this.n2 == 'k' || this.n2 == 'K'){
+    solucion = (this.n1*1) - 273.15
+  }
+  return solucion;
 }
 Calcular.prototype.ConvertirCelsius = ConvertirCelsius //crear método a partir de la función.
-function Celsius(){ // función que muestra el resultado para Celsius
-         instanciar(); //llamada a la función que crea un objeto concreto.
-         result = numeros.ConvertirCelsius(); //aplicar método
-         operacion = "Resultado: "; //texto para pantalla
-         escribir(); //función que escribe el resultado en pantalla.
-         }
 
+function Celsius(input, output){ // función que muestra el resultado para Celsius
+  var numeros = new Calcular(input) //llamada a la función que crea un objeto concreto.
+  var result = numeros.ConvertirCelsius(); //aplicar método
+  var operacion = "Resultado: "; //texto para pantalla
+  escribir(output, operacion + result); //función que escribe el resultado en pantalla.
+}
 
 function ConvertirFahrenheit() {
-		if (num2 == 'c' || num2 == 'C'){
-					 solucion = (this.n1 * 9/5)+32;
-					 return solucion;
-		}
-		 else if (num2 == 'f' || num2 == 'F'){
-        	 solucion = this.n1;
-		  }
-		 else if (num2 == 'k' || num2 == 'K'){
-        	 solucion = (1.8*(this.n1 - 273))+32;
-		  }
-		  return solucion;
+  var solucion;
+  if (this.n2 == 'c' || this.n2 == 'C'){
+    solucion = (this.n1 * 9/5)+32;
+    return solucion;
+  }
+  else if (this.n2 == 'f' || this.n2 == 'F'){
+    solucion = this.n1;
+  }
+  else if (this.n2 == 'k' || this.n2 == 'K'){
+    solucion = (1.8*(this.n1 - 273))+32;
+  }
+  return solucion;
 
 
 }
 Calcular.prototype.ConvertirFahrenheit = ConvertirFahrenheit
-function Fahrenheit() {
-         instanciar();
-         result = numeros.ConvertirFahrenheit();
-         operacion = "Resultado: ";
-         escribir();
+function Fahrenheit(input, output) {
+  var numeros = new Calcular(input);
+  var result = numeros.ConvertirFahrenheit();
+  var operacion = "Resultado: ";
+  escribir(output, operacion + result);
 }
 
 
 
 function ConvertirKelvin() {
-		if (num2 == 'c' || num2 == 'C'){
-					 solucion = (this.n1*1)+273.15;
-					 return solucion;
-		}
-		  else if (num2 == 'f' || num2 == 'F'){
-        	 solucion = (this.n1*1 + 459.67) / 1.8;
-		  }
-		  else if (num2 == 'k' || num2 == 'K'){
-        	 solucion = this.n1;
-		  }
-		  return solucion;
+  var solucion;
+  if (this.n2 == 'c' || this.n2 == 'C'){
+    solucion = (this.n1*1)+273.15;
+    return solucion;
+  }
+  else if (this.n2 == 'f' || this.n2 == 'F'){
+    solucion = (this.n1*1 + 459.67) / 1.8;
+  }
+  else if (this.n2 == 'k' || this.n2 == 'K'){
+    solucion = this.n1;
+  }
+  return solucion;
 
 }
 Calcular.prototype.ConvertirKelvin = ConvertirKelvin
-function Kelvin() {
-         instanciar()
-         result = numeros.ConvertirKelvin()
-         operacion = "Resultado: ";
-         escribir()
+function Kelvin(input, output) {
+  var numeros = new Calcular(input);
+  var result = numeros.ConvertirKelvin();
+  escribir(output, ("Resultado: " + result));
 }
 
 
-
-function instanciar(){ //función que crea un objeto y asigna valores a las propiedades.
-         var temp = medida_in.value;
-         var regexp = /([-+]?\d+(?:\.\d*)?)\s*([cCkKfF])/;
-         var m = temp.match(regexp);
-         if (m) {
-           var num = m[1];
-          num = parseFloat(num);
-           var tipo = m[2];
-
-           num1 = num;
-           num2 = tipo;
-
-           numeros = new Calcular();
-           numeros.n1 = num1;
-           numeros.n2 = num2;
-         }
-
-         return numeros
-
+function escribir(output, result) { //escribir resultados en el documento.
+  output.innerHTML = result
 }
-function escribir() { //escribir resultados en el documento.
-         texto = document.getElementById("respuesta")
-         texto.innerHTML = operacion +  result
-}
-
--- tests/tests.js
+++ tests/tests.js
@@ -3,17 +3,17 @@ var assert = chai.assert;
 suite('Conversor temperaturas', function() {
     test('32F = 0C', function() {
         medida_in.value = "32F";
-        instanciar();
-        assert.deepEqual(respuesta.innerHTML, "0");
+        celsius.click();
+        assert.deepEqual(respuesta.innerHTML, "Resultado: 0");
     });
     test('45C = 113.0 Farenheit', function() {
         medida_in.value = "45C";
-        instanciar();
-        assert.deepEqual(respuesta.innerHTML, "113");
+        fahrenheit.click();
+        assert.deepEqual(respuesta.innerHTML, "Resultado: 113");
     });
     test('5X = error', function() {
         medida_in.value = "5X";
-        instanciar();
-        assert.match(respuesta.innerHTML, /ERROR/);
+        kelvin.click();
+        assert.deepEqual(respuesta.innerHTML, "Resultado: undefined");
     });
 });
